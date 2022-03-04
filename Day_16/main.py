3016d2f09313be98718563e1d71ef74255b43d06
pitrens <pitrens.studio@gmail.com>
2022-03-03 20:48:37 -0500

    code: finish Day_16 challenge

--- Day_16/main.py
+++ Day_16/main.py
@@ -1,3 +1,28 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+
+
+def main():
+  new_order = True
+  menu = Menu()
+  money_machine  = MoneyMachine()
+  coffee_maker = CoffeeMaker()
+  
+  while new_order:
+   order = input(f"\n\nWhat would you like? ({menu.get_items()}): ")
+   match order:
+     case "off":
+       new_order = False
+     case "report":
+       money_machine.report()
+     case _:
+      drink = menu.find_drink(order)
+      if drink and coffee_maker.is_resource_sufficient(drink):
+        is_paid = money_machine.make_payment(drink.cost)
+        if is_paid :
+          coffee_maker.make_coffee(drink)
+
+main()
