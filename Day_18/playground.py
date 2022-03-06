f7c3c01efa4d34963741bb578d06815e699fd765
pitrens <pitrens.studio@gmail.com>
2022-03-05 20:50:09 -0500

    code: draw shapes

--- Day_18/playground.py
+++ Day_18/playground.py
@@ -1,4 +1,9 @@
+import random
 from turtle import Screen, Turtle
+
+
+screen = Screen()
+screen.bgcolor("lightgray")
 
 tim = Turtle()
 tim.shape("turtle")
@@ -25,11 +30,36 @@ tim.color("red")
 
 
 # MAKE DASHED LINE
-for _  in range(15):
-  tim.pendown()
-  tim.forward(10)
-  tim.penup()
-  tim.forward(10)
+# for _  in range(15):
+#   tim.pendown()
+#   tim.forward(10)
+#   tim.penup()
+#   tim.forward(10)
+
+# Draw different shapes based on how many corners
+def draw_shape(corner):
+  angle = TOTAL_ANGLE / corner
+  for _ in range(corner):
+    tim.forward(100)
+    tim.right(angle)
+    
+def change_color():
+    # rand_color = random.choices(range(256), k=3)
+    R = random.random()
+    B = random.random()
+    G = random.random()
+
+    tim.color(R, G, B)
+       
+TOTAL_ANGLE = 360
+MAX_CORNERS = 15
+corner = 3
+
+
+while corner < MAX_CORNERS:
+  change_color()
+  draw_shape(corner)
+  corner += 1
 
 
 
@@ -37,5 +67,6 @@ for _  in range(15):
 
 
 
-screen = Screen()
+
+
 screen.exitonclick()
