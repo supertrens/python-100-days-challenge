61bdba0415640ac6d802a195229acefd1196bec9
pitrens <pitrens.studio@gmail.com>
2022-03-06 20:04:06 -0500

    code: complete random walk

--- Day_18/random_walk.py
+++ Day_18/random_walk.py
@@ -1,11 +1,41 @@
+import random
 from turtle import Turtle , Screen
 
+MAX_REPETITION = 1500
+
+
 tom = Turtle()
-screen = Screen()
-
 tom.shape("turtle")
+tom.pensize(10)
+tom.speed("fastest")
+
+direction = [0, 90, 180, 270]
 
 
+def change_color():
+    # rand_color = random.choices(range(256), k=3)
+    # tom.color(rand_color)
+
+    R = random.random()
+    B = random.random()
+    G = random.random()
+
+    tom.color(R, G, B)
+    
+
+def move():
+  random_direction = random.choice(direction)
+  tom.forward(20)
+  tom.setheading(random_direction)
+
+def main():
+  for _ in range(MAX_REPETITION):
+    change_color()
+    move()
 
 
+# The program starts here
+main()  
+
+screen = Screen()
 screen.exitonclick()
