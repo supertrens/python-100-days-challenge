d789a273158df11cd85e21a0fb7dcc846f3a622e
pitrens <pitrens.studio@gmail.com>
2022-03-11 00:49:53 -0500

    code: check collision with wall

--- Day_22/ball.py
+++ Day_22/ball.py
@@ -10,8 +10,19 @@ class Ball(Turtle):
     self.shape("circle")
     self.color("white")
     self.penup()
+    self.x_move= STEP
+    self.y_move = STEP
    
   def move(self):
-    new_x = self.xcor() + STEP
-    new_y = self.ycor() + STEP
+    self.check_collision_with_wall()
+    
+    new_x = self.xcor() + self.x_move
+    new_y = self.ycor() + self.y_move
     self.goto(x=new_x, y=new_y)
+  
+  def check_collision_with_wall(self):
+    if(self.ycor() > 280 or self.ycor() < -280 ):
+      self.bounce()
+  
+  def bounce(self):
+    self.y_move *= -1
